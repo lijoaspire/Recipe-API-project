c4a1726753296738a00f0ab2b5890b0a7d81e95d
lijoaspire <lijo.george.aspire@gmail.com>
2025-03-15 15:55:59 +0530

    Add code in wait_for_db file and bug fix

--- app/app/calc.py
+++ app/app/calc.py
@@ -1,8 +1,8 @@
 """"
 Calculator functions
-
 """
 
-def add(x,y):
-    """Add x and y and return result """
+
+def add(x, y):
+    """Add x and y and return result"""
     return x + y
--- app/app/test.py
+++ app/app/test.py
@@ -4,10 +4,11 @@ from django.test import SimpleTestCase
 
 from app import calc
 
+
 class CalcTest(SimpleTestCase):
     """Test the calc module"""
 
     def test_add_numbers(self):
-        res = calc.add(5,3)
+        res = calc.add(5, 3)
 
         self.assertEqual(res, 8)
